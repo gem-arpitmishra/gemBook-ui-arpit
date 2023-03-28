0ada11ff5c0f73e667a1f2efb167eca0e5193d21
Arpit Mishra <102647340+gem-arpitmishra@users.noreply.github.com>
2023-03-28 16:00:56 +0530

    Update Hook.java

--- src/main/java/com/qa/gembook/StepDefinition/Hook.java
+++ src/main/java/com/qa/gembook/StepDefinition/Hook.java
@@ -1,19 +1,36 @@
 package com.qa.gembook.StepDefinition;
 
 import com.gemini.generic.exception.GemException;
+import com.gemini.generic.reporting.GemEcoUpload;
 import com.gemini.generic.ui.utils.DriverAction;
 import com.gemini.generic.ui.utils.DriverManager;
+import com.gemini.generic.utils.GemJarGlobalVar;
+import com.gemini.generic.utils.GemJarUtils;
+import com.gemini.generic.reporting.GemTestReporter.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import io.cucumber.java.Before;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hook {
+
     @Before
     public void start() throws GemException {
+//         DriverManager.setUpBrowser();
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--incognito");
+        options.setHeadless(true);
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox");
         DriverManager.initializeChrome(options);
-        DriverAction.maximizeBrowser();
-        DriverAction.launchUrl("https://gembook.geminisolutions.com/#/");
+        DriverAction.launchUrl(GemJarUtils.getGemJarConfigData("launchUrl"));
+        DriverAction.setImplicitTimeOut(Long.parseLong(GemJarGlobalVar.implicitTime));
+        DriverAction.setPageLoadTimeOut(Long.parseLong(GemJarGlobalVar.pageTimeout));
+        DriverAction.setScriptTimeOut(Long.parseLong(GemJarGlobalVar.scriptTimeout));
+
     }
 }
